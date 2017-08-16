16cef3391ae68ae1d476650e4061ca7e87271b8f
James Fonzone <jfonzone@outlook.com>
2017-08-15 23:38:40 -0400

    add background music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// prevents gameObject from being destroyed when a new scene is loaded
+		GameObject.DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
