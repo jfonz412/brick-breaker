a805c73f6ff7e9d001e5f8276cd055355b83005d
James Fonzone <jfonzone@outlook.com>
2017-08-16 23:48:03 -0400

    tidying up

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	//if one object is a trigger (ignore physics)
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print("Trigger");
+		levelManager.LoadLevel("Lose");
+	}
+	
+	//if niether object is a trigger (actual collisions)
+	void OnCollisionEnter2D (Collision2D collision) {
+		Debug.Log("Collider");
+	}
+}
