ed00069f2e3520b12221fb363d94e5b42376fac5
James Fonzone <jfonzone@outlook.com>
2017-08-22 23:46:41 -0400

    move paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -10,6 +10,13 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(Input.mousePosition);
+		Vector3 paddlePos = new Vector3(Mathf.Clamp(8.0f,0.5f,15.5f), this.transform.position.y, 0f);
+		float mousePosInblocks = Input.mousePosition.x / Screen.width * 16; //16 for 16 total blocks *
+
+		paddlePos.x = Mathf.Clamp (mousePosInblocks, 0.5f, 15.5f);
+		this.transform.position = paddlePos;
 	}
 }
+
+//* Background sprite is 800 wide, we set the background image's pixel per world block to 50 pixels per block,
+// 800/50 = 16 blocks
