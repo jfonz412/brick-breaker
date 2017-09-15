92542f5c4ffc41c9e792484bfc2c2fe9c7d10046
James Fonzone <jfonzone@outlook.com>
2017-09-15 17:40:49 -0400

    autoplay

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,18 +2,34 @@
 using System.Collections;
 
 public class Paddle : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	public bool autoPlay = false;
+	private Ball ball;
 	
+	void Start(){
+		ball = GameObject.FindObjectOfType<Ball>();
+		print(ball);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!autoPlay){
+			MoveWithMouse();
+		}else {
+			AutoPlay();
+		}
+	}
+	
+	void MoveWithMouse(){
 		Vector3 paddlePos = new Vector3(Mathf.Clamp(8.0f,0.5f,15.5f), this.transform.position.y, 0f);
-		float mousePosInblocks = Input.mousePosition.x / Screen.width * 16; //16 for 16 total blocks *
-
+		float mousePosInblocks = Input.mousePosition.x / Screen.width * 16; //16 for 16 total blocks *		
 		paddlePos.x = Mathf.Clamp (mousePosInblocks, 0.5f, 15.5f);
+		this.transform.position = paddlePos;
+	}
+	
+	void AutoPlay(){
+		Vector3 paddlePos = new Vector3(Mathf.Clamp(8.0f,0.5f,15.5f), this.transform.position.y, 0f);
+		Vector3 ballPos = ball.transform.position;		
+		paddlePos.x = Mathf.Clamp (ballPos.x, 0.5f, 15.5f);
 		this.transform.position = paddlePos;
 	}
 }
